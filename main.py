2a8b988b7a9ec53a127cc3f97e24edd7f5524c5f
lordplatypus <lordplatypus7@gmail.com>
2025-06-30 11:36:32 -0700

    set up delta time

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ def main():
 
     pygame.init()
     surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta_time = 0
 
     while True:
+        # Get events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-
+        # Draw
         surface.fill("black")
         pygame.display.flip()
+
+        # Delta Time / Framerate
+        delta_time = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
